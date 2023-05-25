abd44d665d728413224fb3542206cf60a4b7b421
hridel.vit <hridel@1webit.cz>
2023-05-25 21:12:19 +0200

    sending mail

--- src/pages/api/contact-form.ts
+++ src/pages/api/contact-form.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 import { ContactFormDataInterface } from '@/global.interface'
+import nodemailer from 'nodemailer'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { token, fullname, email, phone, note } = req.body as ContactFormDataInterface
@@ -10,11 +11,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             response: token
         })
         if (!tokenVerify.data.success) {
-            res.status(500).json({ success: false })
+            res.status(500).json({ success: false, message: 'Nejsi bot?' })
             return
         }
         // send email
-        res.status(200).json({ success: true })
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+        })
+        const mailData = {
+            from: process.env.SMTP_SENDER,
+            to: 'hridel.vit@gmail.com',
+            subject: 'Poptávka z webu',
+            text: `jméno: ${fullname}, email: ${email}${!!phone ? `, tel: ${phone}` : ''}, poznámka: ${note}`,
+            html: `<div><p>Jméno: ${fullname}</p><p>E-mail: ${email}</p>${!!phone ? `<p>Tel: ${phone}</p>` : ''}<p>Poznámka: ${note}</p></div>`
+        }
+        transporter.sendMail(mailData, (err) => {
+            if (err) {
+                res.status(500).json({ success: false, message: 'Nepodařilo se odeslat e-mail' })
+            } else {
+                res.status(200).json({ success: true })
+            }
+        })
     } catch (err: any) {
         res.status(500).json({ success: false })
     }
