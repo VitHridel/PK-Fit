1b725f1d67d53cfcc5a087a5a4717e4b558329d1
hridel.vit <hridel@1webit.cz>
2023-05-27 12:06:41 +0200

    fix sending mails

--- src/pages/api/contact-form.ts
+++ src/pages/api/contact-form.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios'
 import { ContactFormDataInterface } from '@/global.interface'
-import nodemailer from 'nodemailer'
+import nodemailer, { SendMailOptions } from 'nodemailer'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const { token, fullname, email, phone, note } = req.body as ContactFormDataInterface
@@ -11,29 +11,33 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             response: token
         })
         if (!tokenVerify.data.success) {
-            res.status(500).json({ success: false, message: 'Nejsi bot?' })
-            return
+            return res.status(500).json({ success: false, message: 'Nejsi bot?' })
         }
         // send email
         const transporter = nodemailer.createTransport({
+            port: 25,
             host: process.env.SMTP_HOST,
+            secure: false,
+            ignoreTLS: true,
         })
-        const mailData = {
+        const mailData: SendMailOptions = {
             from: process.env.SMTP_SENDER,
-            to: 'hridel.vit@gmail.com',
+            to: process.env.SMTP_SENDER,
+            replyTo: email,
             subject: 'Poptávka z webu',
             text: `jméno: ${fullname}, email: ${email}${!!phone ? `, tel: ${phone}` : ''}, poznámka: ${note}`,
             html: `<div><p>Jméno: ${fullname}</p><p>E-mail: ${email}</p>${!!phone ? `<p>Tel: ${phone}</p>` : ''}<p>Poznámka: ${note}</p></div>`
         }
         transporter.sendMail(mailData, (err) => {
             if (err) {
-                res.status(500).json({ success: false, message: 'Nepodařilo se odeslat e-mail' })
+                console.log('err', err)
+                return res.status(500).json({ success: false, message: 'Nepodařilo se odeslat e-mail' })
             } else {
-                res.status(200).json({ success: true })
+                return res.status(200).json({ success: true })
             }
         })
     } catch (err: any) {
-        res.status(500).json({ success: false })
+        return res.status(500).json({ success: false })
     }
 }
 
